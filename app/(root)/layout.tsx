53357da29a9bb4544c00cdbf505201171141165e
Ben Yip <ben_yip@126.com>
2024-05-19 20:32:34 +0800

    Add some routes and pages

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React, { PropsWithChildren } from "react";
+
+const Layout: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <main className="root">
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
