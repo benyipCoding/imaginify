210b5f3c3b0de20c30153012929e4d9356637842
Ben Yip <ben_yip@126.com>
2024-05-20 22:59:11 +0800

    Add Clerk for Authentication

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,12 @@
+import Sidebar from "@/components/shared/Sidebar";
 import React, { PropsWithChildren } from "react";
 
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <main className="root">
+      {/* Sidebar */}
+      <Sidebar />
+      {/* MobileNav */}
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
