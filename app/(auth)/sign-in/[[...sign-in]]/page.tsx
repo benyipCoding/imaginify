277a9194f805d2f76f764dbdc05ad2855b08d864
Ben Yip <ben_yip@126.com>
2024-05-25 15:01:06 +0800

    Add a bit change

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,19 @@
+import { SignedOut } from "@clerk/clerk-react";
+import { SignInButton, SignedIn } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 import React from "react";
+import Link from "next/link";
 
 const SignInPage = () => {
-  return <div>SignInPage</div>;
+  return (
+    <div>
+      <SignInButton />
+
+      <Button asChild className="button bg-purple-gradient bg-cover">
+        <Link href={"/"}>Home</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default SignInPage;
